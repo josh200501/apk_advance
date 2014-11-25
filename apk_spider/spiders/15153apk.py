fe9d5b565c39d3235f819af3c65270411a26f471
johnson <josh200501@gmail.com>
2014-11-25 15:43:22 +0800

    1. add a android market: www.15153.com

---
+++ apk_spider/spiders/15153apk.py
@@ -0,0 +1,38 @@
+#coding:utf-8
+import scrapy
+from apk_spider.items import APKItem
+from scrapy.contrib.spiders import CrawlSpider,Rule
+from scrapy.contrib.linkextractors import LinkExtractor
+from scrapy.selector import Selector
+import re
+import tools
+import time
+
+class _15153ApkSpider(CrawlSpider):
+    name = '15153'
+    allowed_domains = ['android.15153.com']
+    start_urls = ['http://android.15153.com/soft/','http://android.15153.com/game/']
+
+    rules = (Rule(LinkExtractor(allow=(re.compile(r'http://android.15153.com/soft/list.+'),),)),Rule(LinkExtractor(allow=(re.compile(r'http://android\.15153\.com/app/\d+'),re.compile(r'http://android\.15153\.com/soft/lt/\d+'))),callback='parse_item'),)
+
+    def parse_item(self,response):
+        item = APKItem()
+        sel = Selector(response)
+        name = ''.join(sel.xpath("//div[@class='w360 h20 f18 lh20 ']/h1/text()").extract())
+        size = ''.join(sel.xpath("//div[@class='xqxx_c_c_b']/div[@class='lh20']/text()").extract())
+        url = ''.join(sel.xpath("//div[@class='xqxx_c_d_a']/a/@href").extract())
+        """
+        print "length of size:", len(size)
+        print "size:", size
+        j = 0
+        for i in size:
+            print j, i
+            j += 1
+        """
+        item['name'] = name
+        item['size'] = size[5:]
+        item['url'] = url
+        print item
+        #time.sleep(10)
+        return item
+
